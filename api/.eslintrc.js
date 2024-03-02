a2a796fdf20f34184cc2d2a4573983eb1d44bff9
Halerquinz <daotamkhanh2002@gmail.com>
2024-03-02 22:14:06 +0700

    set up be

---
+++ api/.eslintrc.js
@@ -0,0 +1,47 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "rules": {
+    }
+}
+// module.exports = {
+//     root: true,
+//     parser: "@typescript-eslint/parser",
+//     plugins: ["@typescript-eslint"],
+//     extends: [
+//         "eslint:recommended",
+//         "plugin:@typescript-eslint/recommended",
+//         "prettier",
+//     ],
+//     rules: {
+//         "@typescript-eslint/no-explicit-any": "off",
+//     },
+// };
+
