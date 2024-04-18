a0929e81152fb11ee2f2dc54577c70676f0364a3
Halerquinz <daotamkhanh2002@gmail.com>
2024-04-18 21:32:56 +0700

    Small changes

--- BookingService/migrations/20240407082850_initialization.ts
+++ BookingService/migrations/20240407082850_initialization.ts
@@ -1,9 +1,6 @@
 import type { Knex } from "knex";
 
-
 const TabNameBookingServiceBooking = "booking_service_booking_tab";
-const TabNameBookingServiceBookingSeat = "booking_service_booking_seat_tab";
-const TabNameBookingServicePayment = "booking_service_payment_tab";
 const TabNameBookingServiceFeedback = "booking_service_feedback_tab";
 
 export async function up(knex: Knex): Promise<void> {
@@ -12,47 +9,18 @@ export async function up(knex: Knex): Promise<void> {
             table.increments("booking_id", { primaryKey: true });
             table.integer("of_user_id").notNullable();
             table.integer("of_showtime_id").notNullable();
-
-            table.bigInteger("booking_time").notNullable();
-            table.smallint("booking_status").notNullable();
-            table.integer("amount").notNullable();
-
-            table.index(["status"], "booking_service_booking_status_idx");
-        });
-    }
-
-
-    if (!(await knex.schema.hasTable(TabNameBookingServiceBookingSeat))) {
-        await knex.schema.createTable(TabNameBookingServiceBookingSeat, (table) => {
-            table.increments("of_booking_id");
             table.integer("of_seat_id").notNullable();
 
-            table.foreign("of_booking_id")
-                .references("booking_id")
-                .inTable(TabNameBookingServiceBooking);
+            table.bigInteger("booking_time").notNullable();
+            table.smallint("booking_status").notNullable().defaultTo(0);
+            table.integer("amount").notNullable();
 
-            table.unique(["of_booking_id", "of_seat_id"], {
+            table.unique(["of_user_id", "of_seat_id", "of_showtime_id"], {
                 indexName:
                     "booking_service_booking_seat_idx",
             });
-        });
-    }
 
-    if (!(await knex.schema.hasTable(TabNameBookingServicePayment))) {
-        await knex.schema.createTable(TabNameBookingServicePayment, (table) => {
-            table.increments("payment_id", { primaryKey: true });
-            table.integer("of_booking_id").notNullable();
-
-            table.foreign("of_booking_id")
-                .references("booking_id")
-                .inTable(TabNameBookingServiceBooking)
-
-            table.integer("amount").notNullable();
-            table.smallint("payment_method").notNullable();
-            table.bigInteger("payment_time").notNullable();
-            table.smallint("payment_status").notNullable();
-
-            table.index(["of_booking_id"], "booking_service_payment_of_booking_id_idx");
+            table.index(["booking_status"], "booking_service_booking_status_idx");
         });
     }
 
@@ -76,8 +44,5 @@ export async function up(knex: Knex): Promise<void> {
 
 export async function down(knex: Knex): Promise<void> {
     await knex.schema.dropTableIfExists(TabNameBookingServiceBooking);
-    await knex.schema.dropTableIfExists(TabNameBookingServiceBookingSeat);
     await knex.schema.dropTableIfExists(TabNameBookingServiceFeedback);
-    await knex.schema.dropTableIfExists(TabNameBookingServicePayment);
 }
-
