db8b1d6437f332678127fa51c1439a06a6e8bc7e
Halerquinz <daotamkhanh2002@gmail.com>
2024-04-22 21:14:15 +0700

    Minor Change

--- gateway/src/module/schemas/index.ts
+++ gateway/src/module/schemas/index.ts
@@ -4,7 +4,6 @@ import * as converters from "./converters";
 export * from "./user";
 export * from "./user_role";
 export * from "./user_permission";
-export * from "./user_tag";
 export * from "./movie";
 export * from "./movie_genre";
 export * from "./movie_image";
