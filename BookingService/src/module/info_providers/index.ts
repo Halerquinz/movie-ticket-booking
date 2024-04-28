940a455f1871878abb4574b0a57eb56515b4afce
Halerquinz <daotamkhanh2002@gmail.com>
2024-04-28 11:39:52 +0700

    Remove some file

--- BookingService/src/module/info_providers/index.ts
+++
@@ -1,8 +0,0 @@
-import { Container } from "brandi";
-import { USER_INFO_PROVIDER_TOKEN, UserInfoProviderImpl } from "./user_info_provider";
-
-export * from "./user_info_provider";
-
-export function bindToContainer(container: Container): void {
-    container.bind(USER_INFO_PROVIDER_TOKEN).toInstance(UserInfoProviderImpl).inSingletonScope();
-}
