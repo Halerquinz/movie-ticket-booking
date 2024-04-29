cabb4dd4657338b89806405152527e44694d3275
Halerquinz <daotamkhanh2002@gmail.com>
2024-04-29 07:32:20 +0700

    Small change

--- PaymentService/src/dataaccess/db/index.ts
+++ PaymentService/src/dataaccess/db/index.ts
@@ -1,11 +1,14 @@
 import { Container } from "brandi";
 import { KNEX_INSTANCE_TOKEN, newKnexInstance } from "./knex";
 import { PAYMENT_TRANSACTION_DATA_ACCESSOR_TOKEN, PaymentTransactionDataAccessorImpl } from "./payment_transaction";
+import { CHECKOUT_SESSION_DATA_ACCESSOR_TOKEN, CheckoutSessionDataAccessorImpl } from "./checkout_session";
 
 export * from "./knex";
 export * from "./payment_transaction";
+export * from "./checkout_session";
 
 export function bindToContainer(container: Container): void {
     container.bind(KNEX_INSTANCE_TOKEN).toInstance(newKnexInstance).inSingletonScope();
     container.bind(PAYMENT_TRANSACTION_DATA_ACCESSOR_TOKEN).toInstance(PaymentTransactionDataAccessorImpl).inSingletonScope();
+    container.bind(CHECKOUT_SESSION_DATA_ACCESSOR_TOKEN).toInstance(CheckoutSessionDataAccessorImpl).inSingletonScope();
 }
