54e47b2693519b827438ced35e8355c67790afa5
Halerquinz <daotamkhanh2002@gmail.com>
2024-04-29 07:24:16 +0700

    Implement Payment transaction Data Accessor

--- PaymentService/src/dataaccess/db/payment_transaction.ts
+++ PaymentService/src/dataaccess/db/payment_transaction.ts
@@ -26,17 +26,19 @@ export interface PaymentTransactionDataAccessor {
     createPaymentTransaction(ofBookingId: number, amount: number, requestTime: number, transactionStatus: PaymentTransactionStatus): Promise<number>;
     updatePaymentTransaction(paymentTransaction: PaymentTransaction): Promise<void>;
     getPaymentTransactionWithXLock(id: number): Promise<PaymentTransaction | null>;
+    getPaymentTransactionByBookingIdWithXLock(bookingId: number): Promise<PaymentTransaction | null>;
+    getPaymentTransactionByBookingIdWithStatusPending(bookingId: number): Promise<PaymentTransaction | null>;
+    countPendingPaymentTransaction(ofBookingId: number): Promise<number>;
     withTransaction<T>(cb: (dataAccessor: PaymentTransactionDataAccessor) => Promise<T>): Promise<T>;
 }
 
-const TabNameBookingServicePaymentTransaction = "booking_service_payment_transaction_tab";
-const ColNameBookingServicePaymentTransactionId = "payment_transaction_id";
-const ColNameBookingServicePaymentOfBookingId = "of_booking_id";
-const ColNameBookingServicePaymentAmount = "amount";
-const ColNameBookingServicePaymentRequestTime = "request_time";
-const ColNameBookingServicePaymentUpdateTime = "update_time";
-const ColNameBookingServicePaymentStatus = "status";
-
+const TabNamePaymentServicePaymentTransaction = "payment_service_payment_transaction_tab";
+const ColNamePaymentServicePaymentTransactionId = "payment_transaction_id";
+const ColNamePaymentServicePaymentTransactionOfBookingId = "of_booking_id";
+const ColNamePaymentServicePaymentTransactionAmount = "amount";
+const ColNamePaymentServicePaymentTransactionRequestTime = "request_time";
+const ColNamePaymentServicePaymentTransactionUpdateTime = "update_time";
+const ColNamePaymentServicePaymentTransactionStatus = "status";
 
 export class PaymentTransactionDataAccessorImpl implements PaymentTransactionDataAccessor {
     constructor(
@@ -54,15 +56,15 @@ export class PaymentTransactionDataAccessorImpl implements PaymentTransactionDat
         try {
             const rows = await this.knex
                 .insert({
-                    [ColNameBookingServicePaymentOfBookingId]: ofBookingId,
-                    [ColNameBookingServicePaymentAmount]: amount,
-                    [ColNameBookingServicePaymentStatus]: transactionStatus,
-                    [ColNameBookingServicePaymentRequestTime]: requestTime,
-                    [ColNameBookingServicePaymentUpdateTime]: this.timer.getCurrentTime()
+                    [ColNamePaymentServicePaymentTransactionOfBookingId]: ofBookingId,
+                    [ColNamePaymentServicePaymentTransactionAmount]: amount,
+                    [ColNamePaymentServicePaymentTransactionStatus]: transactionStatus,
+                    [ColNamePaymentServicePaymentTransactionRequestTime]: requestTime,
+                    [ColNamePaymentServicePaymentTransactionUpdateTime]: this.timer.getCurrentTime()
                 })
-                .returning(ColNameBookingServicePaymentTransactionId)
-                .into(TabNameBookingServicePaymentTransaction);
-            return +rows[0][ColNameBookingServicePaymentTransactionId];
+                .returning(ColNamePaymentServicePaymentTransactionId)
+                .into(TabNamePaymentServicePaymentTransaction);
+            return +rows[0][ColNamePaymentServicePaymentTransactionId];
         } catch (error) {
             this.logger.error("failed to create payment transaction", { error });
             throw ErrorWithStatus.wrapWithStatus(error, status.INTERNAL);
@@ -73,16 +75,16 @@ export class PaymentTransactionDataAccessorImpl implements PaymentTransactionDat
     public async updatePaymentTransaction(paymentTransaction: PaymentTransaction): Promise<void> {
         try {
             await this.knex
-                .table(TabNameBookingServicePaymentTransaction)
+                .table(TabNamePaymentServicePaymentTransaction)
                 .update({
-                    [ColNameBookingServicePaymentOfBookingId]: paymentTransaction.ofBookingId,
-                    [ColNameBookingServicePaymentAmount]: paymentTransaction.amount,
-                    [ColNameBookingServicePaymentStatus]: paymentTransaction.status,
-                    [ColNameBookingServicePaymentRequestTime]: paymentTransaction.requestTime,
-                    [ColNameBookingServicePaymentUpdateTime]: this.timer.getCurrentTime()
+                    [ColNamePaymentServicePaymentTransactionOfBookingId]: paymentTransaction.ofBookingId,
+                    [ColNamePaymentServicePaymentTransactionAmount]: paymentTransaction.amount,
+                    [ColNamePaymentServicePaymentTransactionStatus]: paymentTransaction.status,
+                    [ColNamePaymentServicePaymentTransactionRequestTime]: paymentTransaction.requestTime,
+                    [ColNamePaymentServicePaymentTransactionUpdateTime]: this.timer.getCurrentTime()
                 })
-                .where(ColNameBookingServicePaymentTransactionId, "=", paymentTransaction.id)
-                .into(TabNameBookingServicePaymentTransaction);
+                .where(ColNamePaymentServicePaymentTransactionId, "=", paymentTransaction.id)
+                .into(TabNamePaymentServicePaymentTransaction);
         } catch (error) {
             this.logger.error("failed to update payment transaction", {
                 paymentTransaction,
@@ -96,15 +98,15 @@ export class PaymentTransactionDataAccessorImpl implements PaymentTransactionDat
         try {
             const rows = await this.knex
                 .select()
-                .from(TabNameBookingServicePaymentTransaction)
-                .where(ColNameBookingServicePaymentTransactionId, "=", id)
+                .from(TabNamePaymentServicePaymentTransaction)
+                .where(ColNamePaymentServicePaymentTransactionId, "=", id)
                 .forUpdate();
             if (rows.length === 0) {
                 this.logger.debug("no payment transaction with payment_transaction_id found", { paymentTransactionId: id });
                 return null;
             }
             if (rows.length > 1) {
-                this.logger.debug("more than one payment transaction with  payment_transaction_id found", { paymentTransactionId: id });
+                this.logger.debug("more than one payment transaction with payment_transaction_id found", { paymentTransactionId: id });
                 throw new ErrorWithStatus(
                     `more than one payment transaction with payment_transaction_id ${id} found`,
                     status.INTERNAL
@@ -112,7 +114,74 @@ export class PaymentTransactionDataAccessorImpl implements PaymentTransactionDat
             }
             return this.getPaymentTransactionFromRow(rows[0]);
         } catch (error) {
-            this.logger.error("failed to get detection task", { error });
+            this.logger.error("failed to get payment transaction", { error });
+            throw ErrorWithStatus.wrapWithStatus(error, status.INTERNAL);
+        }
+    }
+
+    public async getPaymentTransactionByBookingIdWithXLock(bookingId: number): Promise<PaymentTransaction | null> {
+        try {
+            const rows = await this.knex
+                .select()
+                .from(TabNamePaymentServicePaymentTransaction)
+                .where(ColNamePaymentServicePaymentTransactionOfBookingId, "=", bookingId)
+                .forUpdate();
+            if (rows.length === 0) {
+                this.logger.debug("no payment transaction with booking_id found", { bookingId: bookingId });
+                return null;
+            }
+            if (rows.length > 1) {
+                this.logger.debug("more than one payment transaction with booking_id found", { bookingId: bookingId });
+                throw new ErrorWithStatus(
+                    `more than one payment transaction with booking_id ${bookingId} found`,
+                    status.INTERNAL
+                );
+            }
+            return this.getPaymentTransactionFromRow(rows[0]);
+        } catch (error) {
+            this.logger.error("failed to get payment transaction", { error });
+            throw ErrorWithStatus.wrapWithStatus(error, status.INTERNAL);
+        }
+    }
+
+    public async getPaymentTransactionByBookingIdWithStatusPending(bookingId: number): Promise<PaymentTransaction | null> {
+        try {
+            const rows = await this.knex
+                .select()
+                .from(TabNamePaymentServicePaymentTransaction)
+                .where(ColNamePaymentServicePaymentTransactionOfBookingId, "=", bookingId)
+                .andWhere(ColNamePaymentServicePaymentTransactionStatus, "=", PaymentTransactionStatus.PENDING);
+            if (rows.length === 0) {
+                this.logger.debug("no payment transaction with booking_id with status pending found", { bookingId: bookingId });
+                return null;
+            }
+            if (rows.length > 1) {
+                this.logger.debug("more than one payment transaction with booking_id with status pending found", { bookingId: bookingId });
+                throw new ErrorWithStatus(
+                    `more than one payment transaction with booking_id ${bookingId} with status pending found`,
+                    status.INTERNAL
+                );
+            }
+            return this.getPaymentTransactionFromRow(rows[0]);
+        } catch (error) {
+            this.logger.error("failed to get payment transaction with status pending", { error });
+            throw ErrorWithStatus.wrapWithStatus(error, status.INTERNAL);
+        }
+    }
+
+    public async countPendingPaymentTransaction(ofBookingId: number): Promise<number> {
+        try {
+            const rows = await this.knex
+                .count()
+                .from(TabNamePaymentServicePaymentTransaction)
+                .where(ColNamePaymentServicePaymentTransactionOfBookingId, "=", ofBookingId)
+                .andWhere(ColNamePaymentServicePaymentTransactionStatus, "=", PaymentTransactionStatus.PENDING);
+            return +(rows[0] as any)["count"];
+        } catch (error) {
+            this.logger.error("failed to get pending payment transaction count of booking_id", {
+                ofBookingId,
+                error,
+            });
             throw ErrorWithStatus.wrapWithStatus(error, status.INTERNAL);
         }
     }
@@ -126,12 +195,12 @@ export class PaymentTransactionDataAccessorImpl implements PaymentTransactionDat
 
     private getPaymentTransactionFromRow(row: Record<string, any>): PaymentTransaction {
         return new PaymentTransaction(
-            +row[ColNameBookingServicePaymentTransactionId],
-            +row[ColNameBookingServicePaymentOfBookingId],
-            +row[ColNameBookingServicePaymentAmount],
-            +row[ColNameBookingServicePaymentStatus],
-            +row[ColNameBookingServicePaymentRequestTime],
-            +row[ColNameBookingServicePaymentUpdateTime]
+            +row[ColNamePaymentServicePaymentTransactionId],
+            +row[ColNamePaymentServicePaymentTransactionOfBookingId],
+            +row[ColNamePaymentServicePaymentTransactionAmount],
+            +row[ColNamePaymentServicePaymentTransactionStatus],
+            +row[ColNamePaymentServicePaymentTransactionRequestTime],
+            +row[ColNamePaymentServicePaymentTransactionUpdateTime]
         );
     }
 }
