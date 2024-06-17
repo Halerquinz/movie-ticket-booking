b5d54c319de1ea761f4bb167971cc04b38464b2a
Halerquinz <daotamkhanh2002@gmail.com>
2024-06-17 21:58:08 +0700

    Minor change

--- PaymentService/src/config/stripe.ts
+++ PaymentService/src/config/stripe.ts
@@ -2,7 +2,7 @@ import { token } from "brandi";
 
 export class StripeConfig {
     public apiKey = "sk_test_51P6wPQP8gU29IW1UUuoTwgIr7S53CY3B9bPXkLvOJQAhlW1XeBK95avwaX03VcR760BmvarFq4itDgmt0PCxUf6m00HQ3ObFsm";
-    public webHookEndpointSecret = "whsec_b970a079b33287edad0ef78bce20cc1467f3ae2799f125b7e402d3f3fed06438";
+    public webHookEndpointSecret = "";
     public port = 4242;
 
     public static fromEnv(): StripeConfig {
