e4441035871403c73df8f76d78cedb8c93933548
Halerquinz <85336305+Halerquinz@users.noreply.github.com>
2024-06-17 17:57:35 +0700

    Delete NotificationService directory

--- NotificationService/src/dataaccess/db/index.ts
+++
@@ -1,8 +0,0 @@
-import { Container } from "brandi";
-import { KNEX_INSTANCE_TOKEN, newKnexInstance } from "./knex";
-
-export * from "./knex";
-
-export function bindToContainer(container: Container): void {
-    container.bind(KNEX_INSTANCE_TOKEN).toInstance(newKnexInstance).inSingletonScope();
-}
--- NotificationService/src/dataaccess/elasticsearch/index.ts
+++
@@ -1,8 +0,0 @@
-import { Container } from "brandi";
-import { ELASTICSEARCH_CLIENT_TOKEN, newElasticsearchClient } from "./client";
-
-export * from "./client";
-
-export function bindToContainer(container: Container) {
-    container.bind(ELASTICSEARCH_CLIENT_TOKEN).toInstance(newElasticsearchClient).inSingletonScope();
-}
